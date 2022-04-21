a3702b59ee8d55c47be9f6994d766e568ed9b247
yuchannn <yuchanbpp@gmail.com>
2022-04-21 16:23:54 +0900

    gestures complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion, Variants } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -8,6 +9,17 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+const BiggerBox = styled.div`
+  height: 600px;
+  width: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`
 
 const Box = styled(motion.div)`
   height: 200px;
@@ -20,19 +32,23 @@ const Box = styled(motion.div)`
 const boxVariants: Variants = {
   hover: { scale: 1.5, rotateZ: 90 },
   click: { scale: 1, borderRadius: "100px" },
-  drag: { backgroundColor: "rgb(46, 204, 113)" }, // change color gradually
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVariants}
-        whileHover="hover"
-        whileDrag="drag"
-        whileTap="click"
-      />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag // drag="x" can allow you to drag only in the x-axis
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileTap="click"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
